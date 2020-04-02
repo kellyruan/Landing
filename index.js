436e59aae40788b3cdb6e826ecdc1197cf2e4476
Nick <pc@Nicks-MacBook-Pro.local>
2020-04-02 14:35:32 -0700

    Ready for awesome

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const generatePassword = require('password-generator');
+const Data = require('./data');
 
 const app = express();
 const dbRoute =
