87edc9ccf4486c79d5b7f7427e444c12598a19e8
Nick <pc@Nicks-MacBook-Pro.local>
2020-04-03 18:21:51 -0700

    heroku

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import ReactGA from 'react-ga';
 
 const trackingId = "UA-162746347-1";
 ReactGA.initialize(trackingId);
-ReactGA.pageview("https://projectid.herokuapp.com/");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const customStyles = {
   content : {
